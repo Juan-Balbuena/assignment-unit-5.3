c5d7b3935e0a26c737b4bf28b1ae9e33d54aa8c2
Juan Balbuena <juanbalb48@gmail.com>
2023-09-12 00:36:48 -0500

    completed the final step.

--- assignment/scripts/3-music-collection.js
+++ assignment/scripts/3-music-collection.js
@@ -32,11 +32,32 @@ function showCollection(collection) {
 
 showCollection(myCollection);
 
-// - Create a function named `showCollection`. This function should:
-//   - Take in a `collection` parameter. (This allows it to be reused to show any array of album objects!)
-//   - Loop over the `collection` and `console.log` each album's information formatted **within a single string**, like: `TITLE by ARTIST, published in YEAR`.
 
-// - Test the `showCollection` function.
+function findByArtist(collection, artist){
+  let matchingArtists = [];
+
+  for (let i = 0; i < collection.length; i++) {
+    let record = collection[i];
+    if (record.artist === artist){
+      matchingArtists.push(record);
+    }
+
+  }
+
+  return matchingArtists;
+}
+
+console.log(findByArtist(myCollection, 'Mocha'));
+console.log(findByArtist(myCollection,'Eminem'));
+
+// - Add a function named `findByArtist`. This function should:
+//   - Take in a `collection` parameter. Remember, we want to be able to search any collection!
+//   - Take in an `artist` (string) parameter
+//   - Create an empty array to hold any matching results, if any
+//   - Loop through the `collection` and add any album objects with a matching artist to the array.
+//   - Return the array with the matching results. (If no results are found, an empty array should be returned.)
+
+// - Test the `findByArtist` function. Make sure to test with an artist you know is in the collection, as well as an artist you know is not in your collection. Check that for artists with multiple matches, all are returned.
 
 
 // PLEASE DO NOT MODIFY THIS. Just leave it down here at the bottom. Think of it
